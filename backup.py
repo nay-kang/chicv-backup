4bf56732fc7aa9be47432f2b0485b95409f6e775
chicv <tech@chicv.com>
2015-06-29 00:14:32 +0000

    update date to dattime

--- backup.py
+++ backup.py
@@ -1,20 +1,21 @@
 import os
 import sys
 import json
-from datetime import date
+from datetime import datetime
 from pprint import pprint
 import smtplib
 from email.mime.text import MIMEText
 from subprocess import call
 
 #generat current data string
-today = date.today()
-today_str = today.strftime('%Y%m%d')
-
+today = datetime.now()
+today_str = today.strftime('%Y%m%d_%H%M')
+#current dir
+c_dir=os.path.dirname(os.path.abspath(__file__))+"/"
 
 #send email function
 def sendMail(subject,message):
-    with open("mail.json") as data_file:
+    with open(c_dir+"mail.json") as data_file:
         mailConf = json.load(data_file)
     s = smtplib.SMTP();
     s.connect(mailConf['host'],mailConf['port']);
@@ -24,7 +25,7 @@ def sendMail(subject,message):
 
 #backup database
 def backupDatabase():
-    with open('db.json') as data_file:
+    with open(c_dir+'db.json') as data_file:
         dbConf = json.load(data_file)
     cmd = "%s -u%s -p%s %s > %s-%s.sql"%(dbConf['bin'],dbConf['user'],dbConf['password'],dbConf['database'],dbConf['database'],today_str);
     code = os.system(cmd);
@@ -37,7 +38,7 @@ def backupDatabase():
     code = os.system(cmd);
 
 def rsyncFiles():
-    with open('file.json') as data_file:
+    with open(c_dir+'file.json') as data_file:
         fileConf = json.load(data_file)
     cmd = 'rsync -vrcz --stats %s %s'%(fileConf['source'],fileConf['dest'])
     code = os.system(cmd)
@@ -45,7 +46,7 @@ def rsyncFiles():
         sendMail('file rsync','file rsync failed')
 
 def tarFiles():
-    with open('file.json') as data_file:
+    with open(c_dir+'file.json') as data_file:
         fileConf = json.load(data_file)
     cmd = 'tar zcvf %s/%s-%s.tar.gz %s'%(fileConf['backup_path'],fileConf['name'],today_str,fileConf['source']);
     code = os.system(cmd)
@@ -60,5 +61,5 @@ def uploadToS3():
 backupDatabase()
 rsyncFiles()
 #backup files every thuesday
-if(today.weekday()==1):
+if(today.weekday()==1 and today.hour()==6):
     tarFiles()
